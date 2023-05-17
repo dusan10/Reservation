bf911d04f609311e1a3b08b8afb3e72b27b73b15
dusan <d.pavlovic@netconomy.net>
2023-05-17 13:56:51 +0200

    Car API updated - fixed test

--- src/test/java/com/jimmy/car/reservation/service/impl/ReservationServiceImplIntegrationTest.java
+++ src/test/java/com/jimmy/car/reservation/service/impl/ReservationServiceImplIntegrationTest.java
@@ -1,8 +1,10 @@
 package com.jimmy.car.reservation.service.impl;
 
 import com.jimmy.car.reservation.dao.ReservationRepository;
+import com.jimmy.car.reservation.model.Car;
 import com.jimmy.car.reservation.model.InquiryForCar;
 import com.jimmy.car.reservation.model.Reservation;
+import com.jimmy.car.reservation.service.CarService;
 import com.jimmy.car.reservation.service.ReservationService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
@@ -11,9 +13,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Transactional
@@ -24,6 +30,9 @@ class ReservationServiceImplIntegrationTest {
 
     @Autowired
     private ReservationRepository reservationRepository;
+
+    @Autowired
+    private CarService carService;
 
     @Test
     @DisplayName("Reserve car test real example with good inquiry")
@@ -55,10 +64,23 @@ class ReservationServiceImplIntegrationTest {
     }
 
     @Test
-    @DisplayName("Reserve car test real third inquiry will not be successful during lack of car")
+    @DisplayName("Reserve car test - 3 additional car and 3 inquiry")
     void reserveCarForUseTestThreeInquiry() {
         LocalDateTime start = LocalDateTime.now().plusHours(5);
         int duration = 30;
+
+        List<String> carIds = new ArrayList<>();
+        IntStream.range(0, 3)
+                .forEach(i -> {
+                    Car car = (new Car("TestProducer", "TestModel"));
+                    try {
+                        carService.addCar(car);
+                        carIds.add(car.getId());
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
+                });
+
         InquiryForCar inquiryForCar1 = new InquiryForCar(new Random().nextLong(), start, duration);
         InquiryForCar inquiryForCar2 = new InquiryForCar(new Random().nextLong(), start, duration);
         InquiryForCar inquiryForCar3 = new InquiryForCar(new Random().nextLong(), start, duration);
@@ -68,9 +90,10 @@ class ReservationServiceImplIntegrationTest {
         Optional<Reservation> result2 = reservationService.reserveCarForUse(inquiryForCar2);
         Assertions.assertTrue(result2.isPresent());
         Optional<Reservation> result3 = reservationService.reserveCarForUse(inquiryForCar3);
-        Assertions.assertTrue(result3.isEmpty());
+        Assertions.assertTrue(result3.isPresent());
 
         reservationRepository.delete(result1.get());
         reservationRepository.delete(result2.get());
+        carIds.forEach(id -> carService.removeCar(id));
     }
 }
